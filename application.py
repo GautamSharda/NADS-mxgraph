d3ec0e9cf68af3af976a8ec0c797ae61ff010897
GautamSharda <57648023+GautamSharda@users.noreply.github.com>
2021-01-22 00:17:32 -0600

    Add files via upload

---
+++ application.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+from jinja2 import Markup
+from myParser import device1Name
+
+print(device1Name)
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/devices')
+def device():
+    return render_template("template.html", device=device1Name)
+
+#@app.route('/devices/<device>')
+#def device(device):
+#    return render_template("template.html", device=device)
+
+if __name__== "__main__":
+    app.run()
+
