9a76a1a550c5b98c53bcddebacdabd0b8d8e9bd3
GautamSharda <57648023+GautamSharda@users.noreply.github.com>
2021-02-23 11:56:24 -0600

    Update application.py

--- application.py
+++ application.py
@@ -1,60 +1,39 @@
-from flask import Flask, render_template, url_for
-from jinja2 import Markup
-from myParser import device1Name, devices, tree, etree
+from flask import Flask, render_template, url_for, jsonify
+import json, jsons, jsonpickle
+from lxml import etree
+import numpy as np
+from array import array
+
+#parsing the XML file
+parser = etree.XMLParser(remove_blank_text=True)
+tree = etree.parse('hardware.xml', parser=parser)
+
+#Creating a root
+root = tree.getroot()
+#Defining key elements
+miniSim = root[0]
+hardwareInterface = miniSim[0]
+devices = hardwareInterface[0]
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    myList = []
+    for x in devices:
+        myList.append(x.get("name"))
     return render_template("index.html")
+    return jsonify(myList)
+
 
 
 @app.route('/devices<int:index>')
 def device(index):
     device = devices[index]
-    deviceName = device.get("name")
-    deviceKind = device.get("kind")
-    deviceID = device.get("ID")
-
-    deviceSettings = device[0]
-    deviceSettingsTag = deviceSettings.tag
     
-    deviceSetting1 = deviceSettings[0]
-    deviceSetting1Tag = deviceSetting1.tag
-    deviceSetting1Name = deviceSetting1.get("name")
-    deviceSetting1Type = deviceSetting1.get("type")#unsigned int?
-    deviceSetting1Unit = deviceSetting1.get("unit")
-    deviceSetting1Data = deviceSetting1.text
-    
-    devicePorts = device[1]
-    devicePortsTag = devicePorts.tag
-    
-    devicePort1 = devicePorts[0]
-    devicePort1Tag = devicePort1.tag
-    devicePort1Name = devicePort1.get("name")
-    devicePort1Type = devicePort1.get("type")
-    devicePort1Unit = devicePort1.get("unit")
-    
-    devicePort2 = devicePorts[1]
-    devicePort2Tag = devicePort2.tag
-    devicePort2Name = devicePort2.get("name")
-    devicePort2Type = devicePort2.get("type")
-    devicePort2Unit = devicePort2.get("unit")
-
-    devicePort3 = devicePorts[2]
-    devicePort3Tag = devicePort3.tag
-    devicePort3Name = devicePort3.get("name")
-    devicePort3Type = devicePort3.get("type")
-    devicePort3Unit = devicePort3.get("unit")
-
-    devicePort4 = devicePorts[3]
-    devicePort4Tag = devicePort4.tag
-    devicePort4Name = devicePort4.get("name")
-    devicePort4Type = devicePort4.get("type")
-    devicePort4Unit = devicePort4.get("unit")
-
-    return render_template("template.html", deviceName=deviceName, deviceKind=deviceKind, deviceID=deviceID, settings=deviceSettingsTag, setting1Name=deviceSetting1Name, setting1Type=deviceSetting1Type, setting1Unit=deviceSetting1Unit, setting1Data=deviceSetting1Data, port1Tag=devicePort1Tag, port1Name=devicePort1Name, port1Type=devicePort1Type, port1Unit=devicePort1Unit)
+    return render_template("template.html", deviceName=deviceName)
 
 
 if __name__== "__main__":
     app.run()
+
